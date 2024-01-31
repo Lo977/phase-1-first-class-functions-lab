e5f7442efb0f4db0a86cd89ff87415178a0dacd6
Buddha Lo <lo_977@yahoo.com>
2024-01-31 13:47:36 -0800

    add first class funtins

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const driverList = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(driverList) {
+  return driverList.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(driverList) {
+  return driverList.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function(fare) {
+  return function(par1) {
+  return fare * par1;
+  }
+}
+const fareDoubler = createFareMultiplier(2);
+
+const    fareTripler = createFareMultiplier(3);
+
+const  selectDifferentDrivers = function(driverList, returnLastTwoDrivers) {
+  return returnLastTwoDrivers(driverList);
+}
